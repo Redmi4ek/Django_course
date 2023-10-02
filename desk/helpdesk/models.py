b75b587f0d654dfe3e263ead5f6ea527e7d726b0
unknown <radmirsolo@gmail.com>
2023-10-03 02:19:09 +0600

    Добить переназ у Reception

--- desk/helpdesk/models.py
+++ desk/helpdesk/models.py
@@ -1,21 +1,35 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+STATUS_CHOICES = (
+    ('new', 'New'),
+    ('in_progress', 'In Progress'),
+    ('resolved', 'Resolved'),
+    ('confirmed', 'Confirmed'),
+)
+
+
+
 class HellpDesk(models.Model):
     name = models.CharField(max_length=50) # asker
     phone = models.IntegerField()
     email = models.CharField(max_length=255)
     description = models.TextField()
+    creator_name = models.CharField(max_length=255, default='idontknow')
     CHOICES = [
         ('Low', 'Low'),
         ('Medium', 'Medium'),
         ('High', 'High')
     ]
     priority = models.CharField(max_length=100, choices=CHOICES)
-    status = models.CharField(max_length=250, default="new")
+    status = models.CharField(max_length=250, choices= STATUS_CHOICES , default="new")
     date = models.DateField(default=timezone.now)
     actions_taken = models.TextField(blank=True)
+    assigned_user = models.ForeignKey(User, on_delete=models.SET_NULL , null= True , related_name='assigned_problem_more')
+    resolved_user = models.ForeignKey(User, on_delete=models.SET_NULL , null= True , related_name = 'resolved_problem_more' )
+    confirmed = models.BooleanField(default=False)
 
     def str(self):
         return self.name
--- desk/helpdesk/views.py
+++ desk/helpdesk/views.py
@@ -6,6 +6,9 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from .models import HellpDesk
 from django.urls import reverse
+from django.contrib.auth.models import Group, User
+from django.db.models import Q
+
 
 # Create your views here.
 def login_view(request):
@@ -33,6 +36,11 @@ def add_problem(request):
             help = form.save(commit=False)
             help.creator_name = request.user.username
             help.created_date = timezone.now()
+            reception_group = Group.objects.get(name='Reception')
+            reception_users = reception_group.user_set.all()
+            if reception_users:
+                first_reception_user = reception_users.first()
+                help.assigned_user = first_reception_user
             help.save()
             return redirect('thankyou')
     else:
@@ -43,30 +51,69 @@ def add_problem(request):
 
 @login_required
 def all_problems(request):
-    problem = HellpDesk.objects.all()
+    reception_group = Group.objects.get(name='Reception')
+    tester_group = Group.objects.get(name='Tester')
+    is_reception_user = reception_group.user_set.filter(id=request.user.id).exists()
+    is_tester_user = tester_group.user_set.filter(id=request.user.id).exists()
 
+    if is_reception_user or is_tester_user:
+        if is_reception_user:
+            problems = HellpDesk.objects.filter(assigned_user=request.user)
+        elif is_tester_user:
+            problems = HellpDesk.objects.filter(Q(creator_name=request.user.username) | Q(status='resolved', confirmed=False) )
+    else:
+        problems = HellpDesk.objects.filter(confirmed=False)
 
-    return render(request, 'all_problem.html', {'problems': problem })
+    return render(request, 'all_problem.html', {'is_reception_user': is_reception_user, 'is_tester_user': is_tester_user, 'problems': problems })
 
 def more_details(request, problem_id):
     problem_more = get_object_or_404(HellpDesk, pk=problem_id)
-    
+    is_tester = request.user.groups.filter(name= 'Tester')
+    is_reception = request.user.groups.filter(name= 'Reception')
+    reception_users = User.objects.filter(groups__name='Reception')
+
+
     if request.method == 'POST':
         actions = request.POST.get('actions', '')
         new_status = request.POST.get('status', '')
+        new_assigned_user_id = request.POST.get('assigned_user', None)
+        
+        
+        if is_tester:
+            if new_status == "confirmed":
+                problem_more.delete()
+                return redirect('all_problem')
+            else:
+                problem_more.actions_taken = actions
+                problem_more.status = new_status
 
-        problem_more.actions_taken = actions
-        problem_more.status = new_status
+            if new_assigned_user_id:
+                new_assigned_user = User.objects.get(id=new_assigned_user_id)
+                problem_more.assigned_user = new_assigned_user
+                problem_more.save()
 
-        if new_status == "resolved":
-            problem_more.delete()
-            return redirect('all_problem')
+        elif is_reception:
+            if new_status in ['new', 'in progress']:
+                problem_more.status = new_status
+                problem_more.actions_taken = actions
+                
+                if new_assigned_user_id:
+                    new_assigned_user = User.objects.get(id=new_assigned_user_id)
+                    problem_more.assigned_user = new_assigned_user
+                problem_more.save()
+            
+            elif new_status == 'resolved' and problem_more.status != 'resolved':
+                # Запрет изменения на "resolved", если статус уже "resolved"
+                problem_more.status = new_status
+                problem_more.assigned_user = None
+                problem_more.actions_taken = actions
+                problem_more.save()
 
         problem_more.save()
 
         return redirect(reverse('all_problem'))
 
-    return render(request, 'more.html', {'problem_more': problem_more})
+    return render(request, 'more.html', {'problem_more': problem_more , 'is_tester' : is_tester , 'is_recepption': is_reception , 'reception_users': reception_users})
 
 def thankyou(request):
     return render(request, "thankyou.html")
